9e0923cd46358d4ac300c5f5803f521e5b80c446
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-25 15:00:04 -0300

    Atualizações na UI da section Info.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -29,8 +29,8 @@ export default function Hero() {
         <div className="w-full min-h-screen flex justify-center bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
             <div className="2xl:w-300 min-h-full flex flex-col justify-center items-start 2xl:gap-9 relative">
                 <div className="flex flex-col gap-9 items-start">
-                    <img src={logo} alt="" className="2xl:h-36 w-auto" />
-                    <p className="2xl:max-w-182 dmFont font-semibold 2xl:text-[64px] leading-15 tracking-normal text-[#6A4830]">Awaken the best version of you, take care of your skin, take care of yourself.</p>
+                    <img src={logo} alt="" className="2xl:h-34 w-auto" />
+                    <p className="2xl:max-w-176 dmFont font-semibold 2xl:text-[62px] leading-15 tracking-normal text-[#6A4830]">Awaken the best version of you, take care of your skin, take care of yourself.</p>
                     <p className="2xl:max-w-144 poppinsFont 2xl:font-semibold 2xl:text-lg">With my experience, I’m confident I can help you achieve the skin of your dreams. I can’t wait to welcome you to my treatment room!</p>
                     <button className="flex items-center gap-4 interFont 2xl:font-bold 2xl:text-lg rounded-full px-8 py-3.5 bg-[#6A4830] text-[#FFFFFF]">
                         BOOK MY TREATMENT
@@ -42,8 +42,8 @@ export default function Hero() {
                     <p className="poppinsFont font-bold text-lg leading-[1.44]">Some brands we reached</p>
                     <div className="grid grid-cols-3 gap-8">
                         {
-                            data.map(i => (
-                                <div className="w-full h-62 flex items-end justify-center rounded-[10px] bg-white shadow-[0px_7px_24px_0px_rgba(0,0,0,0.25)]">
+                            data.map((i, index) => (
+                                <div className="w-full h-62 flex items-end justify-center rounded-[10px] bg-white shadow-[0px_7px_24px_0px_rgba(0,0,0,0.25)]" key={index}>
                                     <div className="flex flex-col mb-10">
                                         <p className="interFont font-bold text-[38px] leading-12 text-[#6A4830]">{i.total}</p>
                                         <p className="interFont font-bold text-sm text-[#918F8F]">{i.title}</p>
--- src/sections/Info.tsx
+++ src/sections/Info.tsx
@@ -1,20 +1,53 @@
 import info from "../assets/images/info.png"
 
 export default function Info() {
+
+    const cards = [
+        {
+            step: 1,
+            title: "Personal Assessment",
+            description: "I analyze your skin and body to identify exactly what needs to be treated."
+        },
+        {
+            step: 2,
+            title: "Targeted Treatments",
+            description: "I apply advanced facial and body procedures using professional-grade technology and products."
+        },
+        {
+            step: 3,
+            title: "Real Results",
+            description: "You see and feel the difference healthier skin, improved texture, and a more sculpted, confident body."
+        }
+    ]
+
     return (
-        <div className="w-full 2xl:h-240 flex justify-center bg-[#EFE6DF]">
-            <div className="2xl:w-300 h-full flex flex-col justify-center">
+        <div className="w-full 2xl:h-[116vh] flex justify-center bg-[#EFE6DF]">
+            <div className="2xl:w-300 h-full flex flex-col justify-center gap-6">
                 <div className="flex justify-between items-center">
                     <div className="flex flex-col gap-6">
                         <div className="flex flex-col">
-                            <p className="2xl:w-106 dmFont font-semibold text-[26px] text-black">YOU DON’T HAVE TO LIVE WITH SKIN OR BODY INSECURITIES.</p>
+                            <p className="2xl:w-106 dmFont font-semibold text-[26px] leading-[30px] text-black">YOU DON’T HAVE TO LIVE WITH SKIN OR BODY INSECURITIES.</p>
                             <p className="dmFont font-semibold text-[46px] text-[#6A4830]">I can help you change that.</p>
                         </div>
-                        <p className="2xl:w-118 fontPoppins font-semibold text-sm text-black">If you struggle with acne, pigmentation, dull skin, sagging, localized fat or lack of firmness. I create targeted facial and body treatments designed to correct, improve and restore your confidence. Your concerns are specific. Your treatment should be too.</p>
+                        <p className="2xl:w-118 fontPoppins font-semibold text-sm leading-[22px] text-black">If you struggle with acne, pigmentation, dull skin, sagging, localized fat or lack of firmness. I create targeted facial and body treatments designed to correct, improve and restore your confidence. Your concerns are specific. Your treatment should be too.</p>
                     </div>
-                    <img src={info} alt="" className="size-136"/>
+                    <img src={info} alt="" className="size-136" />
                 </div>
-                <div className="w-full grid grid-cols-3"></div>
+                <div className="w-full grid grid-cols-3 gap-6">
+                    {
+                        cards.map((i, index) => (
+                            <div className="w-full flex flex-col items-center h-60 rounded-xl bg-[#F7F2EE]" key={index}>
+                                <div className="flex flex-col mt-12 max-w-11/20">
+                                    <p className="poppinsFont font-semibold text-[52px] text-[#BF987E]">{i.step}</p>
+                                    <div className="flex flex-col">
+                                        <p className="poppinsFont font-bold text-xl text-black">{i.title}</p>
+                                        <p className="poppinsFont font-bold text-xs text-black">{i.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
