52015c384f5ec04556ff49c536afd5be70c173e0
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-28 09:43:34 -0300

    Pequenas alterações na responsividade da section About.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -27,14 +27,14 @@ export default function Hero() {
 
     return (
         <div className="w-full min-h-screen flex justify-center bg-cover bg-center" style={{ backgroundImage: `url(${background})` }}>
-            <div className="xl:w-216 2xl:w-280 min-h-full flex flex-col justify-center items-start 2xl:gap-9 relative">
+            <div className="xl:w-224 2xl:w-280 min-h-full flex flex-col justify-center items-start 2xl:gap-9 relative">
                 <div className="flex flex-col xl:gap-7.5 2xl:gap-9 items-start absolute xl:top-40 2xl:top-42">
                     <img src={logo} alt="" className="xl:h-14 2xl:h-18 w-auto xl:mb-1 2xl:mb-2" />
                     <p className="xl:max-w-122 2xl:max-w-152 dmFont font-semibold xl:text-[43px] 2xl:text-[54px] xl:leading-12 2xl:leading-15 tracking-normal text-[#6A4830]">Awaken the best version of you, take care of your skin, take care of yourself.</p>
                     <p className="xl:max-w-112 2xl:max-w-xl poppinsFont xl:font-semibold 2xl:font-semibold xl:text-sm 2xl:text-lg">With my experience, I’m confident I can help you achieve the skin of your dreams. I can’t wait to welcome you to my treatment room!</p>
                     <BookMyTreatment bg="bg-[#6A4830]" content="text-[#FFFFFF]"/>
                 </div>
-                <img src={isa} alt="" className="xl:h-238 2xl:h-306 absolute right-0 xl:-bottom-72 2xl:-bottom-88" />
+                <img src={isa} alt="" className="xl:h-242 2xl:h-306 absolute right-0 xl:-bottom-72 2xl:-bottom-88" />
                 <div className="w-full flex flex-col gap-6 2xl:gap-8 absolute -bottom-30 2xl:-bottom-40 z-20">
                     <p className="poppinsFont font-bold 2xl:text-base 2xl:text-lg leading-[1.44]">Some brands we reached</p>
                     <div className="grid grid-cols-3 gap-8">
