d60c1a98ca3ae2abf83b90c69a0fef418596c122
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-28 10:13:16 -0300

    Atualizações na responsividade da section Info.

--- src/sections/Info.tsx
+++ src/sections/Info.tsx
@@ -21,27 +21,27 @@ export default function Info() {
     ]
 
     return (
-        <div className="w-full 2xl:h-[116vh] flex justify-center bg-[#EFE6DF]">
-            <div className="2xl:w-280 h-full flex flex-col justify-center gap-6">
+        <div className="w-full h-[116vh] flex justify-center bg-[#EFE6DF]">
+            <div className="xl:w-226 2xl:w-280 h-full flex flex-col justify-center gap-6">
                 <div className="flex justify-between items-center">
                     <div className="flex flex-col gap-6">
                         <div className="flex flex-col">
-                            <p className="2xl:w-106 dmFont font-semibold text-[26px] leading-7.5 text-black">YOU DON’T HAVE TO LIVE WITH SKIN OR BODY INSECURITIES.</p>
-                            <p className="dmFont font-semibold text-[46px] text-[#6A4830]">I can help you change that.</p>
+                            <p className="xl:w-97 2xl:w-106 dmFont font-semibold xl:text-2xl 2xl:text-[26px] leading-7.5 text-black">YOU DON’T HAVE TO LIVE WITH SKIN OR BODY INSECURITIES.</p>
+                            <p className="xl:w-115.5 2xl:w-132.5 dmFont font-semibold xl:text-[40px] 2xl:text-[46px] text-[#6A4830]">I can help you change that.</p>
                         </div>
-                        <p className="2xl:w-118 fontPoppins font-semibold text-sm leading-5.5 text-black">If you struggle with acne, pigmentation, dull skin, sagging, localized fat or lack of firmness. I create targeted facial and body treatments designed to correct, improve and restore your confidence. Your concerns are specific. Your treatment should be too.</p>
+                        <p className="xl:w-108 2xl:w-118 fontPoppins xl:font-bold 2xl:font-semibold xl:text-xs 2xl:text-sm leading-5.5 text-black">If you struggle with acne, pigmentation, dull skin, sagging, localized fat or lack of firmness. I create targeted facial and body treatments designed to correct, improve and restore your confidence. Your concerns are specific. Your treatment should be too.</p>
                     </div>
-                    <img src={info} alt="" className="size-136" />
+                    <img src={info} alt="" className="xl:size-80 2xl:size-136 " />
                 </div>
-                <div className="w-full grid grid-cols-3 gap-6">
+                <div className="w-full grid grid-cols-3 xl:gap-5 2xl:gap-6">
                     {
                         cards.map((i, index) => (
-                            <div className="w-full flex flex-col items-center h-54 rounded-xl bg-[#F7F2EE]" key={index}>
-                                <div className="flex flex-col mt-8 max-w-12/20">
-                                    <p className="poppinsFont font-semibold text-[52px] text-[#BF987E]">{i.step}</p>
+                            <div className="w-full xl:h-46 2xl:h-54 flex flex-col items-center rounded-xl bg-[#F7F2EE]" key={index}>
+                                <div className="flex flex-col mt-8 max-w-13/20">
+                                    <p className="poppinsFont font-semibold text-5xl 2xl:text-[52px] text-[#BF987E]">{i.step}</p>
                                     <div className="flex flex-col">
-                                        <p className="poppinsFont font-bold text-xl text-black">{i.title}</p>
-                                        <p className="poppinsFont font-bold text-xs text-black">{i.description}</p>
+                                        <p className="poppinsFont font-bold xl:text-[17px] 2xl:text-xl text-black">{i.title}</p>
+                                        <p className="poppinsFont font-bold xl:text-[10px] 2xl:text-xs text-black">{i.description}</p>
                                     </div>
                                 </div>
                             </div>
