efa845ea16bc7425953fe5fe0e00639c467b117b
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-27 21:22:34 -0300

    Atualizações na responsividade do componente Hero.

--- src/components/BookMyTreatment.tsx
+++ src/components/BookMyTreatment.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export default function BookMyTreatment(props: Props) {
     return (
-        <button className={`flex items-center gap-4 interFont font-bold text-lg rounded-full px-7.5 py-3 ${props.bg} ${props.content}`}>
+        <button className={`flex items-center xl:gap-3 2xl:gap-4 interFont font-bold xl:text-base 2xl:text-lg rounded-full xl:px-6.5 2xl:px-7.5 xl:py-3 2xl:py-3.5 ${props.bg} ${props.content}`}>
             BOOK MY TREATMENT
-            <LuCircleArrowRight className={`size-6 ${props.content}`}/>
+            <LuCircleArrowRight className={`xl:size-5 2xl:size-6 ${props.content}`}/>
         </button>
     )
 }
