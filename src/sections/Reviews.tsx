0963df0a52f4fe73e525944cb1134a909a756512
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-28 20:58:37 -0300

    Atualizações na responsividade da section Reviews.

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -6,7 +6,7 @@ export default function Reviews() {
         {
             rating: 5,
             name: "Ana Ichim",
-            review: "Superb visit at Isa Beauty! At 50 years old, the owner has a glowing youthfulness. Excellent facial treatment, I will definitely return!"
+            review: `Superb visit at Isa Beauty! At 50 years old, the owner has a glowing youthfulness. Excellent facial treatment, I will definitely return!`
         },
         {
             rating: 5,
@@ -20,39 +20,48 @@ export default function Reviews() {
         }
     ];
 
-    const getStars = (size: number, color: string) => {
+    const getStars = (size: string, color: string) => {
         return Array.from({ length: 5 }, (_, i) => (
-            <TiStarOutline key={i} className={`size-${size} ${color}`} />
+            <TiStarOutline key={i} className={`${size} ${color}`} />
         ))
     }
 
     return (
-        <div className="w-full 2xl:h-172 flex justify-center bg-[#EFE6DF]">
-            <div className="2xl:w-280 h-full flex flex-col justify-center gap-12">
-                <div className="flex items-center gap-9">
-                    <p className="poppinsFont font-semibold text-[26px] leading-5.5 text-[#6A4830]">Customer reviews</p>
+        <div className="w-full xl:pt-22 2xl:pt-26 xl:pb-12 2xl:pb-18 flex justify-center bg-[#EFE6DF]">
+            <div className="xl:w-226 2xl:w-280 h-full flex flex-col justify-center gap-12">
+                <div className="flex items-center xl:gap-7.5 2xl:gap-9">
+                    <p className="poppinsFont font-semibold xl:text-[24px] 2xl:text-[26px] leading-5.5 text-[#6A4830]">Customer reviews</p>
                     <div className="flex items-center gap-1">
                         {
-                            getStars(4, 'text-[#6A4830]')
+                            getStars('xl:size:3-5 2xl:size-4', 'text-[#6A4830]')
                         }
                     </div>
                 </div>
-                <div className="flex items-center gap-4">
-                    {
-                        reviews.map((i, index) => (
-                            <div className="size-86 flex justify-center relative rounded-[14px] bg-white" key={index}>
-                                <div className="w-4/5 flex flex-col gap-3 absolute top-10">
-                                    <div className="flex items-center">
-                                        {
-                                            getStars(4.5, 'text-yellow-500')
-                                        }
+                <div className="w-full flex flex-col gap-14">
+                    <div className="w-full grid grid-cols-3 items-center gap-4">
+                        {
+                            reviews.map((i, index) => (
+                                <div className="w-full aspect-square flex justify-center relative rounded-[14px] bg-white" key={index}>
+                                    <div className="w-7/10 flex flex-col xl:gap-2.5 2xl:gap-3 absolute top-10">
+                                        <div className="flex items-center">
+                                            {
+                                                getStars('xl:size-4 2xl:size-4.5', 'text-yellow-500')
+                                            }
+                                        </div>
+                                        <p className="poppinsFont font-semibold xl:text-base 2xl:text-lg tracking-wider leading-5.5 text-black">{i.name}</p>
+                                        <p className="poppinsFont font-semibold xl:text-[13px] 2xl:text-sm tracking-wide leading-5.25 text-black">{i.review}</p>
                                     </div>
-                                    <p className="poppinsFont font-semibold text-lg tracking-wider leading-5.5 text-black">{i.name}</p>
-                                    <p className="poppinsFont font-semibold text-sm tracking-wide leading-5.25 text-black">{i.review}</p>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
+                    <div className="w-full flex justify-center xl:gap-3 2xl:gap-3.5">
+                        {
+                            [0, 1, 2].map((i, index) => (
+                                <span className="xl:size-2.5 2xl:size-3 rounded-full bg-white" key={index}/>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
