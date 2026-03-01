230e6dfd4b5f57d186b28d5c4dc9ece10c9aec42
LucasSouza210 <lucassouzafigueiredo112@gmail.com>
2026-02-28 21:52:18 -0300

    Pequenas alterações na UI da section About.

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -20,9 +20,9 @@ export default function About() {
                         </div>
                     </div>
                     <div className="flex-1 flex items-center justify-center rounded-tr-2xl rounded-br-2xl relative bg-[#C89A76]">
-                        <div className="2xl:w-114 flex flex-col items-start xl:gap-5 2xl:gap-7 absolute xl:top-9 2xl:top-9.5">
-                            <p className="xl:w-115 2xl:w-126 dmFont font-semibold xl:text-[21px] 2xl:text-2xl xl:leading-5 2xl:leading-6 tracking-wide text-[#6B452E]">With over 10 years of experience in the beauty industry, I combine professional expertise with a true passion for skin improvement and self-care.</p>
-                            <p className="xl:w-110 2xl:w-120 dmFont xl:font-medium xl:text-xs 2xl:text-sm leading-4 tracking-widest 2xl:tracking-wider xl:mb-2 2xl:mb-0 text-white">Over the years, I have completed multiple courses and earned certifications in different countries, gaining an international perspective on the latest beauty techniques and trends. At IsaBeauty, quality is essential. I work with carefully selected products and treatments that deliver real results, always tailored to your skin and individual needs.</p>
+                        <div className="flex flex-col items-start xl:gap-5 2xl:gap-7 absolute xl:top-9 2xl:top-9.5">
+                            <p className="whitespace-nowrap dmFont font-semibold xl:text-[20px] 2xl:text-2xl xl:leading-5 2xl:leading-6 tracking-wide text-[#6B452E]">With over 10 years of experience in the beauty <br /> industry, I combine professional expertise with <br /> a true passion for skin improvement and self-care.</p>
+                            <p className="whitespace-nowrap dmFont xl:font-medium xl:text-xs 2xl:text-sm leading-4 tracking-wider 2xl:tracking-wider xl:mb-2 2xl:mb-0 text-white">Over the years, I have completed multiple courses and earned certifications in <br /> different countries, gaining an international perspective on the latest beauty <br /> techniques and trends. At IsaBeauty, quality is essential. I work with carefully <br /> selected products and treatments that deliver real results, always tailored to <br /> your skin and individual needs.</p>
                             <BookMyTreatment bg="bg-[#6A4830]" content="text-[#FFFFFF]"/>
                         </div>
                     </div>
